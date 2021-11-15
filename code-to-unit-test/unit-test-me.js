cdd41cb24a037e2c25ec9491679ccf8f17db733e
unknown <jil135@ucsd.edu>
2021-11-14 21:19:55 -0800

    update Readme

--- code-to-unit-test/unit-test-me.js
+++ code-to-unit-test/unit-test-me.js
@@ -1,4 +1,67 @@
-// unit-test-me.js
+// unit.test.js
+
+const functions = require('../code-to-unit-test/unit-test-me.js');
+
+// TODO - Part 2
+
+//test for: isPhoneNumber
+describe("tests for isPhoneNumber",() => {
+    test('test phone number',() => {
+        expect(functions.isPhoneNumber('858-000-1111')).toBe(true);
+    })
+
+    test('test phone number',() => {
+        expect(functions.isPhoneNumber('(858)-000-2222')).toBe(true);
+    })
+
+    test('test phone number',() => {
+        expect(functions.isPhoneNumber('858000000')).toBe(false);
+    })
+
+    test('test phone number',() => {
+        expect(functions.isPhoneNumber('(858)-00-2 ')).toBe(false);
+    })
+})
+
+//test for: isEmail
+describe("tests for isEmail",() => {
+    test('test email',() => {
+        expect(functions.isEmail('test@gmail.com')).toBe(true);
+    })
+
+    test('test email',() => {
+        expect(functions.isEmail('test@ucsd.edu')).toBe(true);
+    })
+
+    test('test email',() => {
+        expect(functions.isEmail('test@.ocm')).toBe(false);
+    })
+
+    test('test email',() => {
+        expect(functions.isEmail('@@@@@@')).toBe(false);
+    })
+})
+ 
+
+//test for: isStrongPassword
+describe("tests for isStrongPassword",() => {
+    test('test password',() => {
+        expect(functions.isStrongPassword('test_1234')).toBe(true);
+    })
+
+    test('test password',() => {
+        expect(functions.isStrongPassword('I_Am_Strong123')).toBe(true);
+    })
+
+    test('test password',() => {
+        expect(functions.isStrongPassword('12345678')).toBe(false);
+    })
+
+    test('test password',() => {
+        expect(functions.isStrongPassword('111111111')).toBe(false);
+    })
+})
+
 
 module.exports = { isPhoneNumber, isEmail, isStrongPassword, isDate, isHexColor };
 
